6818092af7491ad3e2709f6740c29cf0c78d5be9
Jesus Ramirez <79774839+FullFontJ@users.noreply.github.com>
2023-09-01 18:03:53 -0600

    generator password

--- src/App.tsx
+++ src/App.tsx
@@ -4,54 +4,114 @@ import OpcionInclude from './components/OpcionInclude';
 interface CheckboxItem {
   isChecked: boolean;
   label: string;
+  character: string;
 }
 
 function App() {
-  const [sliderValue, setSliderValue] = useState(0);
+  const [logitudPassword, setLogitudPassword] = useState<number>(4);
+  const [passwordNew, setPasswordNew] = useState<string>("");
+  const [stateStrength, setStateStrength] = useState<string>("easy");
+  const [copy, setCopy] = useState<boolean>(false);
   const [checkboxStates, setCheckboxStates] = useState<CheckboxItem[]>([
-    { isChecked: false, label: 'Include Uppercase Letters' },
-    { isChecked: false, label: 'Include Lowercase Letters' },
-    { isChecked: false, label: 'Include Numbers' },
-    { isChecked: false, label: 'Include Symbols' },
+    { isChecked: true, label: 'Include Uppercase Letters', character: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'},
+    { isChecked: false, label: 'Include Lowercase Letters', character: 'abcdefghijklmnopqrstuvwxyz'},
+    { isChecked: false, label: 'Include Numbers', character: '0123456789'},
+    { isChecked: false, label: 'Include Symbols', character: '!@#$%^&*()_-+='},
   ]);
 
+  function passwordStrength(): void{
+    const counterTrue = checkboxStates.filter(objeto => objeto.isChecked === true).length;
+    let strength: string[] = ["easy", "medium", "strong", "robust"];
+    setStateStrength(strength[counterTrue-1]);
+  }
+
+  function characterPassword(): string{
+    let passCharacter: string = "";
+    let indice: number = 0; 
+    while(indice < checkboxStates.length){
+      const item = checkboxStates[indice];
+      if (item.isChecked == true) {
+        passCharacter = passCharacter+""+item.character;
+      }
+      indice++;
+    }
+    return passCharacter;
+  }
+
+  const copyPassword = () => {
+    navigator.clipboard.writeText(passwordNew)
+      .then(() => {
+        setCopy(true);
+      })
+      .catch(() => {
+        setCopy(false);
+      });
+  };
+
+  function generatePassword(caracteresPermitidos: string): string {
+    let password: string = '';
+    const caracteresDisponibles = caracteresPermitidos.length;
   
+    for (let i = 0; i < logitudPassword; i++) {
+      const indiceAleatorio = Math.floor(Math.random() * caracteresDisponibles);
+      password += caracteresPermitidos.charAt(indiceAleatorio);
+    }
+  
+    return password;
+  }
 
   const handleCheckboxChange = (index: number, isChecked: boolean) => {
     const newCheckboxStates = [...checkboxStates];
     newCheckboxStates[index].isChecked = isChecked;
     setCheckboxStates(newCheckboxStates);
+    passwordStrength();
   };
 
   const handleSliderChange = (e: any) => {
-    setSliderValue(e.target.value);
+    setLogitudPassword(e.target.value);
   };
+
+  const showPassword = () =>{
+    let passwordCharacter = characterPassword();
+    let password = generatePassword(passwordCharacter);
+
+    const counterTrue = checkboxStates.filter(objeto => objeto.isChecked === true).length;
+    if (counterTrue < 1) {
+      alert("Select which characters to include");
+    }else{
+        if (logitudPassword < 1) {
+          alert("Change character length");
+        }else{
+          setPasswordNew(password);
+        }
+    }
+    
+  }
 
   return (
     <>
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-customgray mb-4 tracking-widest font-bold text-[20px]">Password Generator</h1>
       <div className="flex flex-row justify-between whitespace-nowrap bg-secundary w-1/3 p-4 rounded-sm gap-2">
-        <input className=' bg-secundary text-customgray text-lg font-bold tracking-tighter w-[90%]' type="text" value="loremloremloremloremloremloremloremlorem" disabled/>
-        <button className='text-customWhite font-light border rounded-lg py-1 px-2'>copy</button>
+        <input className=' bg-secundary text-customgray text-lg font-semibold tracking-tighter w-[90%]' type="text" value={passwordNew} placeholder='Password' disabled/>
+        <button onClick={copyPassword} className='text-customWhite font-light border rounded-lg py-1 px-2 focus:border-primary focus:bg-customWhite focus:text-primary'>copy</button>
       </div>
       <div className="mt-4 flex flex-col gap-8 bg-secundary w-1/3 p-5 rounded-sm ">
           <div className='flex flex-row text-customWhite justify-between'>
             <p className='font-semibold'>Character Length</p>
-            <p className='font-bold text-[25px]'>{sliderValue}</p>
+            <p className='font-bold text-[25px]'>{logitudPassword}</p>
           </div>
           <div className="w-full relative ">
             <input
               type="range"
               min="0"
               max="50"
-              step="0"
-              value={sliderValue}
+              value={logitudPassword}
               onChange={handleSliderChange}
               className="slider appearance-none w-full h-1 bg-black range-input-thumb"
             />
             <div className="absolute left-0 top-1/2 h-2 slider-fill" 
-              style={{ width: `${sliderValue*2}%` }}
+              style={{ width: `${logitudPassword*2}%` }}
             />
           </div>
           <div className='flex flex-col gap-3'>
@@ -61,14 +121,14 @@ function App() {
             title={item.label} 
             isChecked={item.isChecked} 
             onCheckboxChange={(isChecked) => handleCheckboxChange(index, isChecked)}/>
-      ))}
+          ))}
           </div>
 
           <div className='flex flex-row w-full p-4 justify-between bg-primary'>
             <p className='text-customgray text-[16px] font-normal'>STRENGTH</p>
             <div className='flex flex-row gap-2'>
               <p className='text-customWhite'>
-                <strong>MEDIUM</strong>
+                <strong className='uppercase'>{stateStrength}</strong>
               </p>
               <div className='flex flex-row gap-1'>
               {checkboxStates.map((item, index) => {
@@ -88,7 +148,7 @@ function App() {
             </div>
           </div>
 
-          <button className='w-full h-12 font-semibold bg-customWhite text-primary text-[18px]'>
+          <button onClick={showPassword} className='w-full h-12 font-semibold bg-customWhite text-primary text-[18px]'>
             GENERATE
           </button>
 
